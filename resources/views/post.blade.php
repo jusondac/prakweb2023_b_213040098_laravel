496960ad06b4333ef5a4e2c809975c4faa191c58
rejka permana <permanarejka@gmail.com>
2023-11-02 22:27:23 +0700

    Video 8 - Post Category & Eloquent Relationship

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.main')
 
 @section('container')
-<h1 class="mb-5">{{ $post->title }}</h1>
+<h1 class="mb-3">{{ $post->title }}</h1>
+<p>By. Rejka Permana in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
 {{-- {{ $post->body }} --}}
 {!! $post->body !!}
 <a href="/blog">Back to posts</a>
