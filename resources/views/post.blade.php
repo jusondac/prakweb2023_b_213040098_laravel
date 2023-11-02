2d8e6f479e640ca554f72197ac1ac302d263c8f2
rejka permana <permanarejka@gmail.com>
2023-11-03 00:00:38 +0700

    Video 10 - Factory & Faker

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,7 +2,7 @@
 
 @section('container')
 <h1 class="mb-3">{{ $post->title }}</h1>
-<p>By. Rejka Permana in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+<p>By. <a class="text-decoration-none" href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a class="text-decoration-none" href="/categories/{{ $post->category->slug }}"> {{ $post->category->name }}</a></p>
 {{-- {{ $post->body }} --}}
 {!! $post->body !!}
 <a href="/blog" class="d-block mt-3">Back to posts</a>
