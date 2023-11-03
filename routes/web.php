723aed31c7b512d015d91a028b0d87ffd5eda464
rejka permana <permanarejka@gmail.com>
2023-11-03 20:11:18 +0700

    Video 13 - Searching & Pagination

--- routes/web.php
+++ routes/web.php
@@ -34,27 +34,12 @@ Route::get('/about', function () {
     ]);
 });
 
-Route::get('/blog', [PostController::class, 'index']);
-Route::get('posts/{post:slug}', [PostController::class, 'show']);
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/{post:slug}', [PostController::class, 'show']);
 Route::get('/categories', function() {
     return view('categories', [
-        'title' => "Post Categories",
+        'title' => "Post by Categories",
         'active' => 'categories',
         'categories' => Category::all()
-    ]);
-});
-
-Route::get('/categories/{category:slug}', function (Category $category) {
-    return view('posts', [
-        'title' => "Post By Category : $category->name",
-        'active' => 'categories',
-        'posts' => $category->posts->load('category', 'author'),
-    ]);
-});
-Route::get('/authors/{author:username}', function (User $author) {
-    return view('posts', [
-        'title' => "Post By Author : $author->name",
-        'active' => 'categories',
-        'posts' => $author->posts->load('category', 'author'),
     ]);
 });
